767bc6c930f472d023925f7f82ef96057ba8285e
sonoilconte <shammersinger@yahoo.com>
2019-08-06 16:36:13 -0700

    Update mongoose and use global promise library for mongoose

--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/poolapp_test');
+// Use the global NodeJS promise library
+mongoose.Promise = global.Promise;
+const mongoString = process.env.MONGODB_URI || 'mongodb://localhost/poolapp_test';
+mongoose.connect(mongoString, { useNewUrlParser: true });
 
 module.exports.Pool = require('./pool');
 module.exports.Event = require('./event');
